dbb2f4350e60c83bebe149ff5ef8fe020a47ac7f
Park JaeHong <blogerhong@naver.com>
2024-12-03 15:09:03 +0900

    chore : Init Commit

---
+++ src/main/java/com/teamnine/humanofdelivery/HumanOfDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.teamnine.humanofdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanOfDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanOfDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamnine/humanofdelivery/HumanOfDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamnine.humanofdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanOfDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
