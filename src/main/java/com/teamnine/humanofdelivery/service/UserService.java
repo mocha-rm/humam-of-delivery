0b065b32d59016fbe8fcf9a1ccc9e0846be56f23
shine26 <chews26@naver.com>
2024-12-03 21:44:33 +0900

    add : User Service 파일 신규 생성

---
+++ src/main/java/com/teamnine/humanofdelivery/service/UserService.java
@@ -0,0 +1,15 @@
+package com.teamnine.humanofdelivery.service;
+
+public class UserService {
+    // todo 회원가입
+
+    // todo 로그인
+
+    // todo 로그아웃
+
+    // todo 프로필 조회
+
+    // todo 프로필 수정
+
+    // todo 회원 탈퇴
+}
