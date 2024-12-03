419276369d0ff4c86ad8438828e8bb789b9cb706
shine26 <chews26@naver.com>
2024-12-03 17:27:25 +0900

    add : user entity 신규 작성 및 user enum 신규 작성

---
+++ src/main/java/com/teamnine/humanofdelivery/entity/User.java
@@ -0,0 +1,42 @@
+package com.teamnine.humanofdelivery.entity;
+
+import com.teamnine.humanofdelivery.entity.base.BaseEntity;
+import com.teamnine.humanofdelivery.enums.UserRole;
+import com.teamnine.humanofdelivery.enums.UserStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private Long numRestaurants;
+
+}
---
+++ src/main/java/com/teamnine/humanofdelivery/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.teamnine.humanofdelivery.enums;
+
+public enum UserRole {
+    OWNER,
+    USER
+}
