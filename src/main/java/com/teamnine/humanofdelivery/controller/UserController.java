e26fe7a01e0087683a38321e280f51c8a313e4e9
shine26 <chews26@naver.com>
2024-12-03 21:36:55 +0900

    add : User Controller, User Repository 파일 신규 생성

---
+++ src/main/java/com/teamnine/humanofdelivery/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.teamnine.humanofdelivery.controller;
+
+public class UserController {
+    // todo 회원가입
+
+    // todo 로그인
+
+    // todo 로그아웃
+
+    // todo 프로필 조회
+
+    // todo 프로필 수정
+
+    // todo 회원 탈퇴
+}
---
+++ src/main/java/com/teamnine/humanofdelivery/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.teamnine.humanofdelivery.repository;
+
+public interface UserRepository {
+}
