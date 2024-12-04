fa585f83d9d14713a7cd2f8cc9591f9d60eae457
Park JaeHong <blogerhong@naver.com>
2024-12-04 17:27:30 +0900

    fix : 주문 시 현재 시간을 기준으로 영업시간을 확인하도록 로직 수정
    - OrderStatus 값 변경 (COOKED_FINISHED -> COOKED_COMPLETED)
    - StoreStatus 값 변경 (영업종료 삭제, 영업중, 폐업 2개로 관리)

--- src/main/java/com/teamnine/humanofdelivery/service/OrderService.java
+++ src/main/java/com/teamnine/humanofdelivery/service/OrderService.java
@@ -1,7 +1,6 @@
 package com.teamnine.humanofdelivery.service;
 
 import com.teamnine.humanofdelivery.OrderStatus;
-import com.teamnine.humanofdelivery.StoreStatus;
 import com.teamnine.humanofdelivery.dto.OrderRequestDto;
 import com.teamnine.humanofdelivery.dto.OrderResponseDto;
 import com.teamnine.humanofdelivery.entity.Order;
@@ -14,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalTime;
 import java.util.List;
 
 
@@ -29,9 +29,9 @@ public class OrderService {
     @Transactional
     public OrderResponseDto create(OrderRequestDto orderRequestDto) {
         Store findStore = storeRepository.findById(orderRequestDto.getStoreId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 가게를 찾을 수 없습니다."));
-        //TODO : 가게가 영업 중인지, 최소 주문 금액을 만족하는지?
-        if (!findStore.getStatus().equals(StoreStatus.OPEN)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "영업이 종료된 가게입니다.");
+        //TODO : 최소 주문 금액을 만족하는지?
+        if (LocalTime.now().isBefore(findStore.getOpenAt()) || LocalTime.now().isAfter(findStore.getCloseAt())) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "영업시간이 아닙니다.");
         }
 
         Long userId = 1L; //임시 값
