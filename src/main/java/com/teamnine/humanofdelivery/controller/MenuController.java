e3de0462158fd13d97d21b8e9ba84922fbfb7aad
Jun Heo <huhjune98@naver.com>
2024-12-05 19:33:22 -0500

    fix: 메뉴 오류 수정

--- src/main/java/com/teamnine/humanofdelivery/controller/MenuController.java
+++ src/main/java/com/teamnine/humanofdelivery/controller/MenuController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("owner/menus")
+@RequestMapping("/menus")
 public class MenuController {
 
     private final MenuService menuService;
