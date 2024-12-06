e032b0c6bc2076286a9177206b6b54109091c93b
Park JaeHong <blogerhong@naver.com>
2024-12-06 18:09:14 +0900

    fix : Order 도메인 수정(인터셉터에서 user, owner 검증)

--- src/main/java/com/teamnine/humanofdelivery/exception/order/OrderErrorCode.java
+++ src/main/java/com/teamnine/humanofdelivery/exception/order/OrderErrorCode.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum OrderErrorCode {
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 주문을 찾을 수 없습니다."),
-    ORDER_ERROR_AUTHORIZATION_01(HttpStatus.UNAUTHORIZED,"주문은 일반 사용자 계정을 이용해주세요."),
-    ORDER_ERROR_AUTHORIZATION_02(HttpStatus.UNAUTHORIZED,"주문상태를 변경할 권한이 없습니다."),
     ORDER_ERROR_AUTHORIZATION_03(HttpStatus.UNAUTHORIZED, "본인 가게의 주문이 아니므로 상태변경이 제한됩니다."),
     ORDER_ERROR_OWNER_01(HttpStatus.BAD_REQUEST, "같은 상태로는 변경할 수 없습니다."),
     ORDER_ERROR_USER_01(HttpStatus.NOT_ACCEPTABLE, "영업시간 외에는 주문할 수 없습니다."),
